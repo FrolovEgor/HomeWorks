250e82238e6e0908ccdcef4b97bab0020cc05c68
Egor Frolov <frolovegorspb@gmail.com>
2021-01-15 07:32:20 +0300

    HomeWork_2. Added files of home works 2 solution

---
+++ HomeWork_2/NoteBook.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2
+{
+    /// <summary>
+    /// Класс для хранения всех заметок о студентах и вывода заметок в консоль
+    /// </summary>
+    class NoteBook
+    {
+        private List<Student> Notes;
+
+        private void PrintTop()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n{"Имя",15}{"Фамилия",15}{"Возраст",15}{"Рост",15}{"Математика",15}{"Физика",15}{"Язык",15}{"Средний балл",15}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public NoteBook()
+        {
+            Notes = new List<Student>();
+        }
+
+        /// <summary>
+        /// Метод для создания новой заметки
+        /// </summary>
+        /// <returns>Строковое значение об успешном/неуспешном создании новой записи</returns>
+        public string NewNote()
+        {
+            Console.Clear();
+
+            try
+            {
+                Notes.Add(new Student());
+            }
+
+            catch
+            {
+                return "\nЗапись не добавлена, вы ошиблись!(";
+            }
+
+            return "\nЗапись успешно добавлена!";
+        }
+        
+        public string GetNotesCount()
+        {
+            return Notes.Count.ToString();
+        }
+
+
+        /// <summary>
+        /// Метод для печати одной заметки
+        /// </summary>
+        /// 
+        public void PrintOneNote()
+        {
+            Console.Clear();
+            try
+            {
+                Console.Write("Введите номер записи для отображения: ");
+                int noteNumber = int.Parse(Console.ReadLine()) - 1;
+                Console.Write("\n Введите способ вывода(1 - без форматирования, 2 - с форматированием, 3 - С интерполяцией: ");
+                int typeOfPrint = int.Parse(Console.ReadLine());
+
+                switch (typeOfPrint)
+                {
+                    case 1:
+                        Console.WriteLine("{0}\n", Notes[noteNumber].firstName);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].lastName);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].age);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].heiht);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].mathsScore);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].physicsScore);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].langScore);
+                        Console.WriteLine("{0}\n", Notes[noteNumber].averageValue);
+                        break;
+                    case 2:
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].firstName);
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].lastName);
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].age);
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].heiht);
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].mathsScore);
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].physicsScore);
+                        Console.WriteLine("{0, 15}\n", Notes[noteNumber].langScore);
+                        Console.WriteLine("{0:##.##, 15}\n", Notes[noteNumber].averageValue);
+                        break;
+                    case 3:
+                        PrintTop();
+                        Console.WriteLine(Notes[noteNumber].ToString());
+                        break;
+                }    
+            }
+            catch 
+            {
+                Console.WriteLine("\nЧто-то пошло не так. Вы ошиблись!("); ;
+            }            
+        }
+
+        /// <summary>
+        /// Метод для вывода всех записей
+        /// </summary>
+        /// <param name="n"></param>
+        public void PrintAllNotes()
+        {
+            Console.Clear();
+
+            PrintTop();
+            for (int i = 0; i < Notes.Count; i++)
+            {
+                Console.WriteLine(Notes[i].ToString());
+            }
+        }
+    }
+}
---
+++ HomeWork_2/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2
+{
+    /// <summary>
+    /// Класс описывающий модель студента
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// Имя студента
+        /// </summary>
+        public string firstName;
+
+        /// <summary>
+        /// Фамилия студента
+        /// </summary>
+        public string lastName;
+
+        /// <summary>
+        /// Возраст студента
+        /// </summary>
+        public byte age;
+
+        /// <summary>
+        /// рост студента
+        /// </summary>
+        public float heiht;
+        
+        /// <summary>
+        /// Баллы полученные за математику
+        /// </summary>
+        public float mathsScore;
+
+        /// <summary>
+        /// Баллы полученные за физику
+        /// </summary>
+        public float physicsScore;
+
+        /// <summary>
+        /// Баллы полученные за язык
+        /// </summary>
+        public float langScore;
+
+        /// <summary>
+        /// Средний бал за три предмета
+        /// </summary>
+        public float averageValue;
+
+        
+        /// <summary>
+        /// Конструктор заполняющий данные о студенте через консоль
+        /// </summary>
+        public Student()
+        {
+            //Приглашение ввести имя и ввод имени
+            Console.Write("Введите имя: ");
+            firstName = Console.ReadLine();
+
+            //Приглашение ввести фамилию и ввод фамилии
+            Console.Write("Введите Фамилию: ");
+            lastName = Console.ReadLine();
+
+            //Приглашение ввести возраст и ввод возраста
+            Console.Write("Введите возраст: ");
+            age = byte.Parse(Console.ReadLine());
+
+            //Приглашение ввести рост и ввод роста
+            Console.Write("Введите рост: ");
+            heiht = float.Parse(Console.ReadLine());
+
+            //Приглашение ввести балы за математику и ввод данных балов
+            Console.Write("Введите Балы за математику: ");
+            mathsScore = Convert.ToSingle(Console.ReadLine());
+
+            //Приглашение ввести балы за физику и ввод данных балов
+            Console.Write("Введите Балы за физику: ");
+            physicsScore = float.Parse(Console.ReadLine());
+
+            //Приглашение ввести балы за язык и ввод данных балов
+            Console.Write("Введите Балы за язык: ");
+            langScore = float.Parse(Console.ReadLine());
+
+            //Расчет среднего бала студента
+            averageValue = (mathsScore + physicsScore + langScore) / 3;
+ 
+        }
+        /// <summary>
+        /// Метод вывода данных о студенте в консоль
+        /// </summary>
+        public override string ToString()
+        {
+            
+            return ($"{firstName,15}{lastName,15}{age,15}{heiht,15}{mathsScore,15}{physicsScore,15}{langScore,15}{averageValue.ToString("##.##"),15}");
+        }
+    }
+}
