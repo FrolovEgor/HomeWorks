17fdbc1871434f36fc70d031ba7799b17109ce56
Frolov Egor <Heroy318@gmail.com>
2021-05-14 11:39:12 +0300

    HomeWork 7. Created structure PaymentNote, for saving notes information, and class NoteBook, for organization the list of PaymentNote.

---
+++ HomeWork_7/HomeWork_7/NoteBook.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork_7
+{
+    class NoteBook
+    {
+        private List<PaymentNote> _ListOfNotes;
+        public NoteBook() 
+        {
+            _ListOfNotes = new List<PaymentNote>();
+        }
+
+
+
+        public string AddNewNote() 
+        {
+            Console.Write("Введите дату поступления счета: ");
+            DateTime invoiceDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введите номер счета: ");
+            string accountNumber = Console.ReadLine();
+            Console.Write("Введите наименование компании: ");
+            string company = Console.ReadLine();
+            Console.Write("Введите описание счета: ");
+            string description = Console.ReadLine();
+            Console.Write("Комментарий: ");
+            string comment = Console.ReadLine();
+            Console.Write("Введите полную сумму счета: ");
+            double totalPayment = double.Parse(Console.ReadLine());
+            Console.Write("Сколько оплачено на данный момент: ");
+            double nowPaid = double.Parse(Console.ReadLine());
+
+            _ListOfNotes.Add(new PaymentNote(invoiceDate, accountNumber,company,description,comment,totalPayment,nowPaid));
+            return "Запись добавленна";        
+        }
+
+        public void AddInvoiceFile(int numberOfNote) 
+        {
+
+            string path = @"C:\";
+            using (var fileDialog = new OpenFileDialog())
+            {
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                    path = fileDialog.FileName;
+            }
+            _ListOfNotes[numberOfNote].AddInvoiceFile(path);
+            Console.ReadLine();
+
+        }
+        public void OpenInvoiceFile(int numberOfNote)
+        {
+            Console.ReadLine();
+            Process.Start(_ListOfNotes[numberOfNote].PathToInvoiceFile.FullName);           
+        }
+
+
+        
+    }
+}
---
+++ HomeWork_7/HomeWork_7/PaymentNote.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace HomeWork_7
+{
+    struct PaymentNote
+    {
+        public PaymentNote(DateTime invoiceDate, string accountNumber, string company, string description,
+                           string comment, double totalPayment, double nowPaid)
+        {
+            CreationTime = DateTime.Now;
+            InvoiceDate = invoiceDate;
+            AccountNumber = accountNumber;
+            Company = company;
+            Description = description;
+            Comment = comment;
+            PathToInvoiceFile = null;
+            _totalPayment = totalPayment;
+            _nowPaid = nowPaid;
+            _leaveToPay = _totalPayment - _nowPaid;
+        }
+
+        public void ChangePayment(double newPayment)
+        {
+            _nowPaid += newPayment;
+            _leaveToPay = _totalPayment - _nowPaid;
+        }
+        public void AddInvoiceFile(string pathToInvoiceFile)
+        {
+            FileInfo path = new FileInfo(pathToInvoiceFile);
+
+            if (path.Exists)
+            {
+                if (!Directory.Exists($@".\..\..\PaymentDocuments\{Company}\"))
+                {
+                    Directory.CreateDirectory($@".\..\..\PaymentDocuments\{Company}\");
+                }
+                PathToInvoiceFile = new FileInfo($@".\..\..\PaymentDocuments\{Company}\{AccountNumber}{path.Extension}");
+                path.CopyTo(PathToInvoiceFile.FullName, true);
+            }
+
+        }
+
+        public DateTime CreationTime { get; }
+        public DateTime InvoiceDate { get; set; }
+        public string Company { get; set; }
+        public string AccountNumber { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public FileInfo PathToInvoiceFile { get;  set; }
+        public double TotalPayment 
+        { 
+            get
+            {
+                return _totalPayment;
+            }
+            set 
+            {
+                if (value > 0)
+                {
+                    _totalPayment = value;
+                    _leaveToPay = _totalPayment - _nowPaid;
+                }
+            }       
+        }
+        public double NowPaid
+        {
+            get
+            {
+                return _nowPaid;
+            }
+        }
+
+
+        private double _totalPayment;
+        private double _nowPaid;
+        private double _leaveToPay;
+
+    }
+}
