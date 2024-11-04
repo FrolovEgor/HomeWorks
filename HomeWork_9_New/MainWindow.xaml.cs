45ea242dfb1e76297d4d18432a2c01cb193e30e3
Frolov Egor <frolovegorspb@gmail.com>
2024-11-05 01:45:33 +0300

    Added styles for all elements, evaluating by Enter and clearing text boxes when they selected

--- HomeWork_9_New/MainWindow.xaml.cs
+++ HomeWork_9_New/MainWindow.xaml.cs
@@ -22,46 +22,47 @@ namespace HomeWork_9_New
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string[] SplitStr(string Text)
-        {
-            return Text.Split(' ');
-        }
 
-        private ObservableCollection<string> SplitedList = new ObservableCollection<string>();
+        //Collection of splitted words for the visualization 
+        private ObservableCollection<string> _SplitedList = new ObservableCollection<string>();
 
+        //Flags for input clear method
+        private bool _ReverseTextCleared = false;
+        private bool _DevideTextCleared = false;
+        
         public MainWindow()
         {
             InitializeComponent();
-            VievSplitedList.ItemsSource = SplitedList;
+            VievSplitedList.ItemsSource = _SplitedList;  //connecting colletion with visualization
+            this.Resources["ReversalText"] = "";        //Clear initial text from string resiouce
         }
 
+        //Method for spliting the text
+        public string[] SplitStr(string Text)
+        {
+            return Text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        //When split button pressed clear last collection elements and fill it with new words 
         private void Split_Button_press(object sender, RoutedEventArgs e)
         {
-            
+
             if (InputField_Devide.Text != null)
             {
-                SplitedList.Clear();
+                _SplitedList.Clear();
                 string[] SplitedText = SplitStr(InputField_Devide.Text);
 
                 if (SplitedText.Length > 0)
                 {
                     for (int i = 0; i < SplitedText.Length; i++)
                     {
-                        SplitedList.Add(SplitedText[i]);
+                        _SplitedList.Add(SplitedText[i]);
                     }
-
                 }
             }
         }
 
-        private void Split_Button_enter(object sender, KeyEventArgs e) 
-        {
-            if (e.Key == Key.Enter) 
-            {
-                Split_Button_press(sender, e);
-            }
-        }
-
+        //When reverse button pressed generate new reversed string
         private void Reverse_Button_press(object sender, RoutedEventArgs e)
         {
             string FieldText = InputField_Reverse.Text;
@@ -80,15 +81,37 @@ namespace HomeWork_9_New
             }
         }
 
+        //Run split or reverse methods by pressing enter button
+        private void InputField_PressedEnter(object sender, KeyEventArgs e) 
+        {
+            if (e.Key == Key.Enter) 
+            {
+                if (TabSelector.SelectedIndex == 0) Split_Button_press(sender, e);
+                if (TabSelector.SelectedIndex == 1) Reverse_Button_press(sender, e);
+            }
+        }
+
+
+        //Clear the input fields when they are selected first time
+        private void InputField_FirstClear(object sender, RoutedEventArgs e)
+        {
+            if (TabSelector.SelectedIndex == 0 && _DevideTextCleared == false)
+            {
+                InputField_Devide.Clear();
+                _DevideTextCleared = true;
+            }
+
+            if (TabSelector.SelectedIndex == 1 && _ReverseTextCleared == false)
+            {
+                InputField_Reverse.Clear();
+                _ReverseTextCleared = true;
+            }
+        }
+
+        //Close the app
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
-        private void InputField_Devide_TextChanged(object sender, TextChangedEventArgs e)
-        {
-        
-        }
-
     }
 }
